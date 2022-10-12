4ab94578c1bd766a06da464082b1427c0b9e9bbf
Hamid Dev <protiox.pro@gmail.com>
2022-10-12 07:43:54 +0200

    Juste une commit

--- OneDrive/Cours/Languages/TP5/exo2.c
+++ OneDrive/Cours/Languages/TP5/exo2.c
@@ -1,24 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
 int main(){
-
-    int i,n;
+    int num, len=0;
+    int i,j,c,n;
+    char incr;
     char chaine;
-    puts("Veuillez entrer le nombre de caractères");
+    puts("Veuillez entrer le nombre de caractères à entrer");
     scanf("%d", &n);
-    int *tab = malloc(n * __SIZEOF_INT__);
-    char *chaine = malloc(n * __SIZEOF_WCHAR_T__);
+    int *tab;
+    char *chai;
 
+    
+    len=(num==0)?1:log10(num)+1;  
+
+    tab = malloc((len+1)*sizeof(int));
+    chai = malloc((len+1)*sizeof(char));
+
+
+    while (i<5 && j<5)
+    {
+        if (a[i]==b[j]){
+            c=1;
+        }else
+        {
+            c=0;
+        }
+        i++;
+        j++;
+    }
     for (i=0;i!=n;i++)
     {
-        puts("Veuillez entrer la note ");
-        scanf("%s", &chaine[i]);
+        puts("Veuillez entrer le chiffre");
+        for 
+        scanf("%c", &chai[i]);
     }
 
     for (i=0;i!=n;i++)
     {
-        tab[i] = chaine[i];
+        tab[i] = chai[i];
     }
     printf("%d",&tab);
 }
